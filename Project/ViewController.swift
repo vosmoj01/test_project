3567fbfac8012eda65ba5962a454a0c5427ae0c0
Алексей Вадимович <aleksejvadimovic@MacBook-Pro-Aleksej-2.local>
2024-02-15 17:18:07 +0300

    create commit

---
+++ Project/ViewController.swift
@@ -0,0 +1,71 @@
+//
+//  ViewController.swift
+//  Project
+//
+//  Created by Алексей Вадимович on 30/01/2024.
+//
+
+import UIKit
+import SnapKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupViewController()
+        addSubviews()
+        addConstraints()
+        
+        navigationItem.title = "navigationVC"
+        navigationController?.toolbar.isTranslucent = false
+    }
+
+    private let orangeViewMain: UIView = {
+        let viewMain = UIView()
+        viewMain.backgroundColor = .orange
+        return viewMain
+    }()
+
+    private let redButton: UIButton = {
+        let button = UIButton()
+        button.backgroundColor = .red
+        button.layer.cornerRadius = 16
+        button.setTitle("Press me, please", for: .normal)
+        button.addTarget(self, action: #selector(buttonDidTapped), for: .touchUpInside)
+        return button
+    }()
+
+    private func setupViewController() {
+        view.backgroundColor = .white
+    }
+
+    private func addSubviews() {
+        view.addSubview(orangeViewMain)
+        view.addSubview(redButton)
+    }
+
+    @objc
+    func buttonDidTapped() {
+        print(1111)
+    }
+
+    private func addConstraints() {
+        orangeViewMain.snp.makeConstraints { make in
+            make.left.equalToSuperview().offset(100)
+            make.top.equalToSuperview().offset(100)
+            make.height.equalTo(200)
+            make.width.equalTo(200)
+        }
+
+        redButton.snp.makeConstraints { make in
+            make.left.equalToSuperview().offset(8)
+            make.right.equalToSuperview().offset(-8)
+            make.height.equalTo(56)
+            make.bottom.equalToSuperview().offset(-100)
+        }
+    }
+
+
+}
+
